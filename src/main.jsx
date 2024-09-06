287c50a046b8c7a28b874d75b2214365f58d0c64
Alif Hossain <alifhossain56782@gmail.com>
2024-09-06 16:13:09 +0600

    Headline Journal Project Route Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import routes from './routes/Routes.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {routes}></RouterProvider>
   </StrictMode>,
 )
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layouts/Root";
+import Home from "../Pages/Home";
+
+const routes = createBrowserRouter([
+
+{
+    path: "/",
+    element:<Root></Root>,
+    children: [
+
+        {
+            path: '/',
+            element: <Home></Home>
+
+        }
+
+
+
+
+    ]
+      
+    
+}
+
+
+
+
+
+]);
+
+export default routes;
